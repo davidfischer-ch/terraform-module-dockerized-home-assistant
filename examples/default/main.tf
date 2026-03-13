415d1f11fff4549b28bd05c16a8d59280d1071e9
David Fischer <david@fisch3r.net>
2026-03-13 13:32:57 +0100

    Remove redundant default values from examples and READMEs

--- examples/default/main.tf
+++ examples/default/main.tf
@@ -7,7 +7,6 @@ module "home_assistant" {
   source = "git::https://github.com/davidfischer-ch/terraform-module-dockerized-home-assistant.git?ref=1.0.1"
 
   identifier     = "home-assistant"
-  enabled        = true
   image_id       = docker_image.home_assistant.image_id
   data_directory = "/data/home-assistant"
 
